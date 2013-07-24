1328c37d219eb36076adf3c8fba6217cf4906f93
Stuart Runyan <shrunyan@gmail.com>
2013-07-23 21:59:54 -0700

    More backbone reading and learning

--- app.js
+++ app.js
@@ -8,27 +8,92 @@
  */
 (function(){
 
-    var app ={};
+    var app = {};
+          //notes = [];
 
     app.Note = Backbone.Model.extend({
+        //localStorage: new Backbone.LocalStorage('notes-backbone'),
         defaults: function(){
-            console.log('Note:defaults');
             return {
+                id: 1,
                 title: 'New Note',
                 content: 'Hi, this is a new note.'
             };
         },
+        initialize: function() {
+            console.log('Note Created');
+
+            // Listen for changes
+            this.on('change:title', function() {
+                this.save();
+            });
+            this.on('change:content', function() {
+                this.save();
+            });
+            this.on("invalid", function(model, error){
+                console.log(error);
+            });
+        },
         save: function() {
             console.log('Note:save');
-            console.log(this.content);
-        },
-        load: function() {
-            console.log('Note:load');
-            return this.content;
+
+            // Remove note
+            //localStorage.removeItem(this.get('title'));
+            localStorage.removeItem(this.get('title'));
+
+            // Re-Add note
+            localStorage.setItem(this.get('title'), this.get('content'));
         }
     });
+    //var note = new app.Note();
+    //note.set('title', 'testing out a new');
+    //note.set('content', 'changing the content test');
 
-    /*app.NoteList = Backbone.Collection.extend({
+    /*notes.push(new app.Note());
+    notes.push(new app.Note());
+    notes.push(new app.Note());
+
+    console.log(notes);*/
+
+
+
+    app.NoteView = Backbone.View.extend({
+        tagName: 'li',
+        notes_list: $('#notes'),
+        template: _.template('<a href="#load" class="load"><%- title %></a>'),
+        events: {
+            'click .load' : 'load',
+            'dblclick .load' : 'edit',
+            'keypress .edit': 'updateOnEnter'
+        },
+        initialize: function() {
+            console.log('NoteView:initialize');
+            //this.listenTo(this.model, 'change', this.render);
+            this.render();
+        },
+        render: function() {
+            console.log('NoteView:render');
+            var html = this.model.toJSON();
+            console.log(html);
+            //console.log(this.model.get('title'));
+            //var link = this.$el.html();
+            //this.$('p').prop();
+            return this;
+        },
+        edit: function() {
+            console.log('NoteView:edit');
+        },
+        load: function() {
+            console.log('NoteView:load');
+            //$('#editor textarea').val(this.get('content'));
+        }
+    });
+    //var noteView = new app.NoteView();
+
+
+
+
+    app.Notes = Backbone.Collection.extend({
         model: app.Note,
         localStorage: new Backbone.LocalStorage('notes-backbone'),
         open: function() {
@@ -36,29 +101,25 @@
             $('#editor textarea').val(this.model.content);
         },
         save: function() {
-
+            //save this collection
         }
     });
-    app.Notes = new app.NoteList();*/
+    // var ntoes = new Backbone.collection();
+    var notes = new app.Notes();
+    notes.add([
+        {id: 1, title: 'tet ad test', content: 'fuck dis'},
+        {id: 2, title: 'tetaa   aef test', content: 'fuck dis'},
+        {id: 3, title: 'tet te a df st', content: 'fuck dis'}
+    ]);
 
-    /*app.NoteView = Backbone.View.extend({
-        tagName: 'li',
-        //template: _.template($('#note-template').html()),
-        events: {
-            'click .load' : 'loadNote',
-            'click .save' : 'saveNote'
-        },
-        initialize: function() {
-            console.log('NoteView:initialize');
-            this.listenTo(this.model, 'change', this.render);
-        },
-        render: function() {
-            console.log('AppView:render');
-            this.$el.html('<p>Test render</p>');
-            //this.$('p').prop();
-            return this;
-        }
-    });*/
+    console.log(notes.length);
+
+    notes.forEach(function(model){
+        console.log(model.get('title'));
+    });
+
+    console.log(notes.pluck('content'));
+
 
 
     app.AppView = Backbone.View.extend({
@@ -97,6 +158,8 @@
         },
         newNote: function() {
             console.log('AppView:newNote');
+            var note = new app.Note();
+
         },
         menu: function() {
             console.log('AppView:menu');
@@ -109,6 +172,11 @@
     /**
      * Party Time!!!
      */
-    app.appView = new app.AppView();
+    //app.appView = new app.AppView();
+
+
+    /*return {
+        notes: notes
+    };*/
 
 })();
