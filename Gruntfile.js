d481167ab94d3b97a95d2bd3c8a3f9e1d3e43071
Stuart Runyan <shrunyan@gmail.com>
2014-05-04 21:01:30 -0700

    Build updates

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,14 +2,13 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'), 
+    pkg: grunt.file.readJSON('package.json'),
   });
 
   grunt.loadTasks('build/');
 
-  grunt.registerTask('dev', ['clean', 'copy', 'jshint', 'connect:dev', 'watch']);
-  grunt.registerTask('build', []);
-  grunt.registerTask('prod', []);
+  grunt.registerTask('dev', ['clean', 'copy', 'cssmin', 'jshint', 'connect:dev', 'watch']);
+  grunt.registerTask('prod', ['clean', 'copy', 'jshint']);
 
   grunt.registerTask('default', ['dev']);
 
