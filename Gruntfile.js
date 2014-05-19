18695218f036413eb2f9c6a112018ae45543bdf2
Stuart Runyan <shrunyan@gmail.com>
2014-05-19 09:43:28 -0700

    working on javascript build

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,12 +3,25 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    requirejs: {
+        compile: {
+            options: {
+                baseUrl: './',
+                mainConfigFile: 'src/requirejs/config.js',
+                dir: 'prod/',
+                optimize: "uglify",
+                useStrict: true
+            }
+        }
+    }
   });
+
+  grunt.loadNpmTasks('grunt-contrib-requirejs');
 
   grunt.loadTasks('build/');
 
   grunt.registerTask('dev', ['clean', 'copy', 'cssmin', 'jshint', 'connect:dev', 'watch']);
-  grunt.registerTask('prod', ['clean', 'copy', 'jshint']);
+  grunt.registerTask('prod', ['clean', 'copy', 'jshint', 'concat:prod']);
 
   grunt.registerTask('default', ['dev']);
 
